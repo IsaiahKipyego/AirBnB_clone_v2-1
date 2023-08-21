f3c48968bdb5096ed9efc361fa42700fbd83feef
FREDRICKKYEKI <fredrickisaac142@gmail.com>
2023-08-22 00:54:33 +0300

    [update]: task 5 of ./fred_tests/console.py

--- fred_test/models/engine/file_storage.py
+++ fred_test/models/engine/file_storage.py
@@ -8,8 +8,15 @@ class FileStorage:
     __file_path = 'file.json'
     __objects = {}
 
-    def all(self):
+    def all(self, cls=None):
         """Returns a dictionary of models currently in storage"""
+        if cls != None:
+            newobjs = {}
+            for key, val in FileStorage.__objects.items():
+                if isinstance(val, cls):
+                   newobjs[key] = val
+            return newobjs
+
         return FileStorage.__objects
 
     def new(self, obj):
@@ -24,6 +31,15 @@ class FileStorage:
             for key, val in temp.items():
                 temp[key] = val.to_dict()
             json.dump(temp, f)
+
+    def delete(self, obj=None):
+        """ Deletes obj from __objects if it's inside
+        if equal to None, the method should not do anything"""
+        if obj == None:
+            return
+        if obj in FileStorage.__objects.values():
+            key = "{}.{}".format(obj.__class__.__name__, obj.id)
+            del FileStorage.__objects[key]
 
     def reload(self):
         """Loads storage dictionary from file"""
