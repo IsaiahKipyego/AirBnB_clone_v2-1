8655bec8ab36ea229f139fbebc669a6ed68f6ed1
NamasakaLennox <namasakalennox254@gmail.com>
2023-09-07 22:21:43 +0300

    advanced task for cleaning oudated archives

--- 100-clean_web_static.py
+++ 100-clean_web_static.py
@@ -2,32 +2,25 @@
 """
 cleans out of date archives
 """
-import os
-from fabric.api import env, run, run_once
+from fabric.api import *
 
-env.hosts = ["54.227.128.86", "54.237.40.109"]
+
+env.hosts = ['35.231.33.237', '34.74.155.163']
+env.user = "ubuntu"
 
 
 def do_clean(number=0):
-    """Deletes out-of-date archives of the static files.
-    Args:
-        number (Any): The number of archives to keep.
     """
-    archives = os.listdir('versions/')
-    archives.sort(reverse=True)
-    start = int(number)
-    if not start:
-        start += 1
-    if start < len(archives):
-        archives = archives[start:]
+    Cleans outdated files
+    """
+
+    number = int(number)
+
+    if number == 0:
+        number = 2
     else:
-        archives = []
-    for archive in archives:
-        os.unlink('versions/{}'.format(archive))
-    cmd_parts = [
-        "rm -rf $(",
-        "find /data/web_static/releases/ -maxdepth 1 -type d -iregex",
-        " '/data/web_static/releases/web_static_.*'",
-        " | sort -r | tr '\\n' ' ' | cut -d ' ' -f{}-)".format(start + 1)
-    ]
-    run(''.join(cmd_parts))
+        number += 1
+
+    local('cd versions ; ls -t | tail -n +{} | xargs rm -rf'.format(number))
+    path = '/data/web_static/releases'
+    run('cd {} ; ls -t | tail -n +{} | xargs rm -rf'.format(path, number))
