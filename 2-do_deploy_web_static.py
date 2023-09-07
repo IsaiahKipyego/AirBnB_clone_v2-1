577dd46ab05cce487a9ca127a1ee7d39f426e105
NamasakaLennox <namasakalennox254@gmail.com>
2023-09-07 20:54:22 +0300

    changed deploy

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -3,34 +3,33 @@
 sets up server for deployment
 """
 from fabric.api import env
-from fabric.operations import run, local, put
-from fabric.context_managers import settings
-from os import path
-
-
+from fabric.operations import run, put
+from os.path import exists
 env.hosts = ["54.227.128.86", "54.237.40.109"]
+
 
 def do_deploy(archive_path):
     """
     deploys a version of the file to the server
     """
-    if not path.exists(archive_path):
+    if not exists(archive_path):
         return False
     try:
         put(archive_path, '/tmp/')
-        folder = archive_path.replace('versions/', '')
-        folder = folder.replace('.tgz', '')
-        run("mkdir -p /data/web_static/releases/{}/".format(folder))
-        run("tar -xzf /tmp/{}.tgz -C /data/web_static/releases/{}/"
-            .format(folder, folder))
-        run("rm /tmp/{}.tgz".format(folder))
-        run("cp -r /data/web_static/releases/{}/web_static/*\
-        /data/web_static/releases/{}/".format(folder, folder))
-        run("rm -rf /data/web_static/releases/{}/web_static".format(folder))
-        run("rm -rf /data/web_static/current")
-        run("ln -sfn /data/web_static/releases/{}/ /data/web_static/current"
-            .format(folder))
+        name = archive_path.split("/")[-1]
+        no_ext = name.split('.')[0]
+        path = "/data/web_static/releases/"
+        # upload the contents
+        put(archive_path, '/tmp/')
+        # setup the directories
+        run('mkdir -p {}{}/'.format(path, no_ext))
+        run('tar -xzf /tmp/{} -C {}{}/'.format(name, path, no_ext))
+        run('rm /tmp/{}'.format(name))
+        run('mv {0}{1}/web_static/* {0}{1}/'.format(path, no_ext))
+        run('rm -rf {}{}/web_static'.format(path, no_ext))
+        run('rm -rf /data/web_static/current')
+        run('ln -s {}{}/ /data/web_static/current'.format(path, no_ext))
+        return True
 
     except Exception as e:
         return False
-    return True
