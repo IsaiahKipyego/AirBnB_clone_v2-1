9607959180e5e4947e4e21fe500606accac34c43
NamasakaLennox <namasakalennox254@gmail.com>
2023-09-24 15:56:44 +0300

    prints all cities in all states available

---
+++ web_flask/8-cities_by_states.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""
+Starts a flask web application for the airbnb template
+"""
+from flask import Flask, render_template
+from models import storage
+from models.state import State
+
+app = Flask(__name__)
+
+
+@app.route('/cities_by_states', strict_slashes=False)
+def cities_by_states():
+    """
+    displays the states in the database
+    """
+    states = storage.all(State).values()
+    states = sorted(states, key=lambda state: state.name)
+
+    return render_template('8-cities_by_states.html', states=states)
+
+
+@app.teardown_appcontext
+def teardown(self):
+    storage.close()
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
